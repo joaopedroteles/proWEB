b57118699482cb137ad3c74f44ea594c525be158
João Pedro de Oliveira Teles <80984493+joaopedroteles@users.noreply.github.com>
2023-09-05 21:09:03 -0300

    Add files via upload

---
+++ AT4/atividade4/calculadoraWeb.js
@@ -0,0 +1,36 @@
+/*não compreendi direito a tarefa, muito provável que esteja errado :(*/
+
+const calc = require('./util/calculadora');
+const express = require('express');
+
+const app = express();
+
+
+app.get("/somar/:a/:b", function (req, res) {
+    let a = req.params.a;
+    let b = req.params.b;
+    res.send(`${a} + ${b} = ${calc.somar(a, b)}`);
+});
+
+app.get("/subtrair/:a/:b", function (req, res) {
+    let a = req.params.a;
+    let b = req.params.b;
+    res.send(`${a} - ${b} = ${calc.subtrair(a, b)}`);
+});
+
+app.get("/multiplicar/:a/:b", function (req, res) {
+    let a = req.params.a;
+    let b = req.params.b;
+    res.send(`${a} * ${b} = ${calc.multiplicar(a, b)}`);
+});
+
+app.get("/dividir/:a/:b", function (req, res) {
+    let a = req.params.a;
+    let b = req.params.b;
+    res.send(`${a} / ${b} = ${calc.dividir(a, b)}`);
+});
+
+const PORT = 8080;
+app.listen(PORT, function () {
+    console.log("app rodando na porta "+PORT);
+});
